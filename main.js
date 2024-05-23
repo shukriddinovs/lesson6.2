a7733692ac9c39918cfd55956b19e45bef8811f2
shukriddinovs <sshukriddinov524@gmail.com>
2024-05-23 14:16:20 +0500

    {.}

---
+++ main.js
@@ -0,0 +1,20 @@
+let name1 = prompt("Ismingizni kiriting")
+let spaceld = prompt("Space id kiriting")
+let posword = prompt("Space paswordingizni kiriting")
+
+const dasturchi = {
+    name: name1,
+    id :spaceld,
+    pasword : posword
+}
+console.log(dasturchi);
+
+// const maktab = {
+//    oquvchilar : 500,
+//    oqitivchilar:50,
+//    zal :true,
+//    stadion :false,
+//    xonalarda : ["parta" ,"stol","stul"],
+// };
+// maktab.milliygvardiya =true;
+// console.log(maktab.milliygvardiya);
